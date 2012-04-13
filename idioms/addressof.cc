e1d2ee9798faec6366fa2d30095130c07fc98ecb
bitdewy <bitdewy@gmail.com>
2012-04-14 00:31:06 +0800

    C++ code snippet

---
+++ idioms/addressof.cc
@@ -0,0 +1,22 @@
+class nonaddressable
+{
+public:
+  typedef double useless_type;
+private:
+  useless_type operator&() const;
+};
+
+//similar in boost
+//http://svn.boost.org/svn/boost/trunk/boost/utility/addressof.hpp
+template <typename T>
+T* addressof(T& v)
+{
+  return reinterpret_cast<T*>(
+    &const_cast<char&>(reinterpret_cast<const volatile char&>(v)));
+}
+int main(void)
+{
+  nonaddressable na;
+  nonaddressable* naptr = addressof(na);
+  return 0;
+}
