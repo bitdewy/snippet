a71b9f36f9abed59cef0373db3f82f0f6e5d619d
bitdewy <bitdewy@gmail.com>
2013-10-24 11:15:09 +0800

    add switcher

---
+++ utility/switcher.hpp
@@ -0,0 +1,33 @@
+#ifndef BITDEWY_SWITCHER_HPP_
+#define BITDEWY_SWITCHER_HPP_
+
+namespace bitdewy {
+template <int index, int count, class dispatcher>
+struct switcher {
+  typename dispatcher::result_type
+  operator()(const dispatcher& disp, int type) const {
+    if (type == index) {
+      return disp.template Case<index>();
+    } else {
+      return switcher<index + 1, count, dispatcher>()(disp, type);
+    }
+  }
+};
+
+template <int count, class dispatcher>
+struct switcher<count, count, dispatcher> {
+  typename dispatcher::result_type
+  operator()(const dispatcher& disp, int type) const {
+    return disp.Default(type);
+  }
+};
+
+template <int count, class dispatcher>
+typename dispatcher::result_type
+Switch(const dispatcher& disp, int type) {
+  return switcher<0, count, dispatcher>()(disp, type);
+}
+
+}  // namespace bitdewy
+
+#endif  // BITDEWY_SWITCHER_HPP_
