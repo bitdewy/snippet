308e284bcef3f9661d8c19fa6eb235aad514f2e5
bitdewy <bitdewy@gmail.com>
2013-12-30 11:38:36 +0800

    add thread raii

---
+++ idioms/threadraii.cpp
@@ -0,0 +1,26 @@
+#include <thread>
+
+class ThreadRAII {
+public:
+  typedef void (std::thread::*RAIIAction)();  // dtor action
+  ThreadRAII(std::thread&& thread, RAIIAction a)
+    : t(std::move(thread)), action(a) {}
+
+  ~ThreadRAII() {
+    if (t.joinable()) { (t.*action)(); }
+  }
+
+  std::thread& get() { return t; }
+private:
+  RAIIAction action;
+  std::thread t;
+};
+
+void doThisWork() {}
+void doThatWork() {}
+
+int main(int argc, char* argv[]) {
+  ThreadRAII t1(std::thread(doThisWork), &std::thread::join);
+  ThreadRAII t2(std::thread(doThatWork), &std::thread::detach);
+  return 0;
+}
